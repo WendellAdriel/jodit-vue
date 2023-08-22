519d7b90c6d7162ed739854e08b44b851fec7ced
Leon Dore <leon.dore@gmail.com>
2023-08-22 08:51:06 -0400

    - Fixed issue in watcher that was referencing a non-existent property - Update Jodit instantiator

--- src/JoditEditor.vue
+++ src/JoditEditor.vue
@@ -3,58 +3,60 @@
 </template>
 
 <script>
-import { Jodit } from 'jodit'
+import { Jodit } from "jodit";
 
 export default {
-  name: 'JoditEditor',
+  name: "JoditEditor",
 
   props: {
     modelValue: { type: String, required: true },
     buttons: { type: Array, default: null },
     extraButtons: { type: Array, default: null },
     config: { type: Object, default: () => ({}) },
-    plugins: { type: Array, default: () => [] }
+    plugins: { type: Array, default: () => [] },
   },
 
-  emits: ['update:modelValue'],
+  emits: ["update:modelValue"],
 
   data: () => ({ editor: null }),
 
   computed: {
-    editorConfig () {
-      const config = { ...this.config }
+    editorConfig() {
+      const config = { ...this.config };
 
       if (this.buttons) {
-        config.buttons = this.buttons
-        config.buttonsMD = this.buttons
-        config.buttonsSM = this.buttons
-        config.buttonsXS = this.buttons
+        config.buttons = this.buttons;
+        config.buttonsMD = this.buttons;
+        config.buttonsSM = this.buttons;
+        config.buttonsXS = this.buttons;
       }
 
-      if (this.extraButtons) config.extraButtons = this.extraButtons
-      return config
-    }
+      if (this.extraButtons) config.extraButtons = this.extraButtons;
+      return config;
+    },
   },
 
   watch: {
-    value (newValue) {
-      if (this.editor.value !== newValue) this.editor.value = newValue
-    }
+    modelValue(newValue) {
+      if (this.editor.value !== newValue) this.editor.value = newValue;
+    },
   },
 
-  mounted () {
+  mounted() {
     if (this.plugins.length) {
-      this.plugins.forEach(plugin => {
-        Jodit.plugins.add(plugin.name, plugin.callback)
-      })
+      this.plugins.forEach((plugin) => {
+        Jodit.plugins.add(plugin.name, plugin.callback);
+      });
     }
-    this.editor = new Jodit(this.$el, this.editorConfig)
-    this.editor.value = this.modelValue
-    this.editor.events.on('change', newValue => this.$emit('update:modelValue', newValue))
+    this.editor = Jodit.make(this.$el, this.editorConfig);
+    this.editor.value = this.modelValue;
+    this.editor.events.on("change", (newValue) =>
+      this.$emit("update:modelValue", newValue)
+    );
   },
 
-  beforeUnmount () {
-    this.editor.destruct()
-  }
-}
+  beforeUnmount() {
+    this.editor.destruct();
+  },
+};
 </script>
